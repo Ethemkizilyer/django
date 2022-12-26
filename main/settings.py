7e0e0feff0fcf9a2d5d2312ee9bb2223eeb196d7
Ethm KZLYR <106928233+Ethemkizilyer@users.noreply.github.com>
2022-12-26 05:35:49 +0300

    serializers start

--- main/settings.py
+++ main/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-eyx*k)#-^3h)mac5m^ve8!aqm=ysl@4_v(x44unb+b^)lbgb%%'
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -39,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bakar',
     'relation',
+    'student_api',
 ]
 
 MIDDLEWARE = [
