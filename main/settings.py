c9e2c6250208ca22493057497607ff6cb2454611
Ethm KZLYR <106928233+Ethemkizilyer@users.noreply.github.com>
2022-12-26 14:23:33 +0300

    restframework install

--- main/settings.py
+++ main/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'bakar',
     'relation',
     'student_api',
+    'rest_framework', 
 ]
 
 MIDDLEWARE = [
--- main/urls.py
+++ main/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from bakar.views import home
 from django.conf import settings
 from django.conf.urls.static import static
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
  
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",home)
+    path("",home),
+    path("api/",include("student_api.urls")),
 ]
 
 
